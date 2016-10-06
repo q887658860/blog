c08e84c07e6b768da6043fee4b759efda21fe98a
WUKAN-PC\Kan <q887658860@163.com>
2016-10-07 04:40:31 +0800

    set port to 80

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var MongoStore = require('connect-mongo')(session);
 var app = express();
 
 // view engine setup
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(flash());
